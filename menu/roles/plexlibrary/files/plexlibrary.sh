b5ab23f2ce16b10760a14d3d76e77304bec6823a
hawkinzzz <hawkinzzz@hotmail.co.uk>
2020-01-11 15:20:23 +0000

    correct file path

--- menu/roles/plexlibrary/files/plexlibrary.sh
+++ menu/roles/plexlibrary/files/plexlibrary.sh
@@ -12,12 +12,12 @@
 PATH='/usr/bin:/bin:/usr/local/bin'
 export PYTHONIOENCODING=UTF-8
 echo $(date) | tee -a /opt/appdata/python-plexlibrary/plexlibrary.log
-echo "" | tee -a /opt/python-plexlibrary/plexlibrary.log
+echo "" | tee -a /opt/appdata/python-plexlibrary/plexlibrary.log
 
 for file in /opt/appdata/python-plexlibrary/recipes/*
 do
     if [ ! -d "${file}" ]; then
-        /usr/bin/python3 /opt/appdata/python-plexlibrary/plexlibrary/plexlibrary.py $(basename "$file" .yml) | tee -a /opt/python-plexlibrary/plexlibrary.log
+        /usr/bin/python3 /opt/appdata/python-plexlibrary/plexlibrary/plexlibrary.py $(basename "$file" .yml) | tee -a /opt/appdata/python-plexlibrary/plexlibrary.log
         echo "" | tee -a /opt/appdata/python-plexlibrary/plexlibrary.log
     fi
 done
